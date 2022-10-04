21d61a69a1f920815e026fb62d842dccf2cb0830
Kuldeep Upreti <kuldeep.upreti@coloredcow.com>
2022-10-04 17:07:33 +0530

    :art: Added quill and webpack server

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import Quill from "quill";
+import QuillCursors from "quill-cursors";
+import * as Y from "yjs";
+import { QuillBinding } from "quill-binding";
+
+Quill.register('modules/cursors', QuillCursors);
+
+const quill = new Quill(document.querySelector('#editor'), {
+    modules: {
+        cursors: true,
+        toolbar: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline'],
+            ['image', 'code-block']
+        ],
+        history: {
+            userOnly: true
+        }
+    },
+    placeholder: 'Start collaborating...',
+    theme: 'bubble'
+});
+
+// A yjs document holds the shared data.
+const ydoc = new Y.Doc();
+
+// Define a shared text type on the document.
+const ytext = ydoc.getText('quill');
+
+// Create an editor-binding which "binds" the quill editor to a Y.Text type.
+const binding = new QuillBinding(ytext, quill);
