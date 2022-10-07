66669550c1230f19a7b8be83ddca041bd0e36baf
Kuldeep Upreti <kuldeep.upreti@coloredcow.com>
2022-10-07 18:07:10 +0530

    :art: Updated editor preferences

--- src/index.js
+++ src/index.js
@@ -1,52 +1,40 @@
-import * as Y from "yjs";
-import { WebrtcProvider } from "y-webrtc";
-import { QuillBinding } from "y-quill";
-import Quill from "quill";
-import QuillCursors from "quill-cursors";
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import { QuillBinding } from 'y-quill'
+import Quill from 'quill'
+import QuillCursors from 'quill-cursors'
 
-Quill.register('modules/cursors', QuillCursors);
+Quill.register('modules/cursors', QuillCursors)
 
 const quill = new Quill(document.querySelector('#editor'), {
-    modules: {
-        cursors: true,
-        toolbar: [
-            [{ header: [1, 2, false] }],
-            ['bold', 'italic', 'underline'],
-            ['image', 'code-block']
-        ],
-        history: {
-            userOnly: true
-        }
-    },
-    placeholder: 'Start collaborating...',
-    theme: 'snow'
-});
-// A yjs document holds the shared data.
-const ydoc = new Y.Doc();
-const provider = new WebrtcProvider('quill-demo-room', ydoc);
+  modules: {
+    cursors: true,
+    toolbar: [
+      // adding some basic Quill content features
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block']
+    ],
+    history: {
+      // Local undo shouldn't undo changes
+      // from remote users
+      userOnly: true
+    }
+  },
+  placeholder: 'Start collaborating...',
+  theme: 'snow' // 'bubble' is also great
+})
 
-// All of our network providers implement the awareness crdt.
-const awareness = provider.awareness;
+// A Yjs document holds the shared data
+const ydoc = new Y.Doc()
 
-// You can observe when a user updates their awareness information.
-awareness.on('change', changes => {
-    // Whenever somebody updates their awareness information, we log all awareness information from all users.
-    console.log(Array.from(awareness.getStates().values()));
-});
+const provider = new WebrtcProvider('quill-demo-room', ydoc)
 
-// You can think of your own awareness information as a key-value store. We update our "user" field to propagate relevant user information.
+// Define a shared text type on the document
+const ytext = ydoc.getText('quill')
 
-awareness.setLocalStateField('user', {
-    // Define a print name that should be displayed.
-    name: 'Kuldeep Upreti',
-    // Define a color that should associated to the user:
-    color: '#ffb61e'
-});
+// "Bind" the quill editor to a Yjs text type.
+const binding = new QuillBinding(ytext, quill, provider.awareness)
 
-
-
-// Define a shared text type on the document.
-const ytext = ydoc.getText('quill');
-
-// Create an editor-binding which "binds" the quill editor to a Y.Text type.
-const binding = new QuillBinding(ytext, quill, provider.awareness);
+// Remove the selection when the iframe is blurred
+window.addEventListener('blur', () => { quill.blur() })
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     mode: 'development',
     entry: {
         index: './src/index.js',
-        print: './src/print.js',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -14,27 +13,16 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Development',
+            filename: './index.html',
         }),
     ],
+    optimization: {
+        runtimeChunk: 'single',
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, "dist"),
         clean: true,
         publicPath: '/',
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
     },
 };
